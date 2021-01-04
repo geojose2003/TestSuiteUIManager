45dd78d7e0b062de72d7bfa118c50dabfd76e304
reddy676 <76934143+reddy676@users.noreply.github.com>
2021-01-04 18:08:53 +0530

    updated login ts file

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -5,8 +5,7 @@ import { NgxSpinnerService } from "ngx-spinner";
 
 @Component({
   selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  templateUrl: './login.component.html'
 })
 export class LoginComponent implements OnInit {
 
